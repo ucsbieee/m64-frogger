558e58fa84f7c7484cd10cc1f93fcc01db6727d9
E4tHam <ethan@sifferman.dev>
2022-04-30 16:40:18 -0700

    added `fill_obmas` methods

--- screen.c
+++ screen.c
@@ -26,3 +26,24 @@ void screen_randomize(void){
     }
 
 }
+
+void row_clear_ombas(row_t * row) {
+    obma_stack_push(row->ombas, row->num_obmas);
+    row->num_obmas = 0;
+}
+void row_fill_obmas(row_t * row) {
+    uint8_t i;
+    row_clear_ombas(row); // reset rows
+    for (i = 0; i < 16; i++) {
+        row->num_obmas += (layouts[row->layout] != TILE_EMPTY);
+        // row->num_obmas stores number of non-empty tiles
+    }
+    row->num_obmas *= 4; // tiles are 4x4
+    obma_stack_pop(row->num_obmas, row->num_obmas);
+}
+void screen_fill_obmas(void) {
+    uint8_t i;
+    for (i = 0; i < 15; i++) {
+        row_fill_obmas(screen + i);
+    }
+}
--- screen.h
+++ screen.h
@@ -1,15 +1,22 @@
 #include "layout.h"
 
-#ifndef SCREEN_H
-#define SCREEN_H
+#ifndef __SCREEN_H
+#define __SCREEN_H
 
 typedef struct row_s {
-    const uint8_t layout;
+    uint8_t layout;
     uint8_t offset;
     sint8_t velocity;
+    uint8_t ombas[64];
+    uint8_t num_obmas;
 } row_t;
 
 extern row_t screen[15];
 
 void screen_randomize(void);
+
+void row_clear_ombas(row_t * row);
+void row_fill_obmas(row_t * row);
+void screen_fill_obmas(void);
+
 #endif
