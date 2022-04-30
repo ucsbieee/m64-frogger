6e80cb8dbddffbe160a810442ca072b41b11111e
jananiganesh7 <91993459+jananiganesh7@users.noreply.github.com>
2022-04-30 16:30:01 -0700

    Added screen randomize

--- screen.c
+++ screen.c
@@ -1,3 +1,28 @@
 #include "screen.h"
+#include <stdlib.h>
 
 row_t screen[15];
+
+void screen_randomize(void){
+    int i = 0;
+    int var;
+    for(i =0; i < 15; i++){
+        screen[i].layout = rand()%num_layouts;
+    }
+
+    for(i=0; i<15; i++){
+        var = rand()%4;
+        if(var == 0){
+            var = -1;
+        }
+        else if(var == 3){
+            var = -2;
+        }
+        screen[i].velocity = var;
+    }
+
+    for(i=0; i<15; i++){
+        screen[i].offset = rand();
+    }
+
+}
--- screen.h
+++ screen.h
@@ -4,11 +4,12 @@
 #define SCREEN_H
 
 typedef struct row_s {
-    const layout_t *layout;
+    const uint8_t layout;
     uint8_t offset;
     sint8_t velocity;
 } row_t;
 
 extern row_t screen[15];
 
+void screen_randomize(void);
 #endif
