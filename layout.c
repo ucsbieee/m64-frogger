c258139270a3368164538f1a5fba726e46c09fc2
E4tHam <ethan@sifferman.dev>
2022-05-02 11:47:43 -0700

    improved formatting

--- layout.c
+++ layout.c
@@ -1,26 +1,13 @@
 #include "layout.h"
 
 const layout_t layouts[] = {
-    {TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY,
-     TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY,
-     TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY},
     {
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-        TILE_CAR,
-        TILE_EMPTY,
-    }};
+        TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY,
+        TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY, TILE_EMPTY
+    }, {
+        TILE_CAR,   TILE_EMPTY, TILE_CAR,   TILE_EMPTY, TILE_CAR,   TILE_EMPTY, TILE_CAR,   TILE_EMPTY,
+        TILE_CAR,   TILE_EMPTY, TILE_CAR,   TILE_EMPTY, TILE_CAR,   TILE_EMPTY, TILE_CAR,   TILE_EMPTY
+    }
+};
 
-const uint8_t num_layouts = 1;
+const uint8_t num_layouts = 2;
--- layout.h
+++ layout.h
@@ -4,11 +4,11 @@
 
 #include <int.h>
 
-#define TILE_EMPTY ((uint8_t)0)
-#define TILE_TURTLE ((uint8_t)1)
-#define TILE_LOG ((uint8_t)2)
-#define TILE_CAR ((uint8_t)3)
-#define TILE_GATOR ((uint8_t)4)
+#define TILE_EMPTY  ((uint8_t)0)
+#define TILE_TURT   ((uint8_t)1)
+#define TILE_LOG    ((uint8_t)2)
+#define TILE_CAR    ((uint8_t)3)
+#define TILE_GATOR  ((uint8_t)4)
 
 typedef uint8_t layout_t[16];
 
